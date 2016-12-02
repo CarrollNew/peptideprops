1dccab4967738328c89d4127c75154675afa226b
Rail Suleymanov <rail.suleymanov@gmail.com>
2016-12-02 15:17:10 +0400

    Separate AA/mRNA (#16)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from json import dumps, loads
 
 from sequence_property_wrapper import ProteinPropertyWrapper, RNAPropertyWrapper
-from seq_utils import rna_to_aa
+from seq_utils import rna_to_aa, AA_LETTERS, RNA_LETTERS
 
 
 def _read_params(fn):
@@ -11,37 +11,48 @@ def _read_params(fn):
 		return loads(f.read())
 
 
-def write_result(out_fn, result):
+def _write_result(out_fn, result):
 	with open(out_fn, 'w') as f:
 		f.write(dumps(result, indent=4))
 
 
 def process(params):
-	if 'sequence' not in params:
+	sequence = params.get('sequence', None)
+	sequence_type = params.get('type', None)
+	folding_temp = params.get('folding_temp', None)
+	if not sequence:
 		return {'error': 'Invalid parameters', 'message': 'No mRNA sequence provided.'}
-	if 'folding_temp' not in params:
-		return {'error': 'Invalid parameters', 'message': 'No settings provided.'}
-	rna_seq = params['sequence']
-	results = []
-	try:
-		aa_seq = rna_to_aa(rna_seq)
-	except StandardError as e:
-		return {'error': 'Invalid mRNA sequence.', 'message': e.message}
-
-	protein_analysis = ProteinPropertyWrapper(aa_seq)
-	results.extend(protein_analysis.molecular_weight())
-	results.extend(protein_analysis.composition())
-	results.extend(protein_analysis.extinction_coefficient())
-	results.extend(protein_analysis.absorption_coefficient())
-	results.extend(protein_analysis.half_life())
-	results.extend(protein_analysis.instability_index())
-	results.extend(protein_analysis.isoelectric_point())
-	results.extend(protein_analysis.aliphatic_index())
-	results.extend(protein_analysis.gravy())
-	rna_analysis = RNAPropertyWrapper([rna_seq])
-	results.extend(rna_analysis.folding(temp=params['folding_temp']))
-	result_dict = {'sequence': aa_seq, 'results': results}
-	return result_dict
+	if not sequence_type:
+		return {'error': 'Invalid parameters', 'message': 'No sequence type provided.'}
+	results = {}
+	if sequence_type == 'mRNA':
+		if not all(map(lambda let: let in RNA_LETTERS, sequence)):
+			return {'error': 'Not an RNA sequence.'}
+		rna_results = []
+		rna_results = RNAPropertyWrapper([sequence]).folding(temp=folding_temp)
+		results[sequence_type] = rna_results
+		try:
+			sequence = rna_to_aa(sequence)
+			sequence_type = 'Protein'
+		except StandardError as e:
+			return results
+	if sequence_type == 'Protein':
+		if not all(map(lambda let: let in AA_LETTERS, sequence)):
+			return {'error': 'Not a protein sequence.'}
+		protein_results = []
+		protein_analysis = ProteinPropertyWrapper(sequence)
+		protein_results.extend(protein_analysis.molecular_weight())
+		protein_results.extend(protein_analysis.composition())
+		protein_results.extend(protein_analysis.extinction_coefficient())
+		protein_results.extend(protein_analysis.absorption_coefficient())
+		protein_results.extend(protein_analysis.half_life())
+		protein_results.extend(protein_analysis.instability_index())
+		protein_results.extend(protein_analysis.isoelectric_point())
+		protein_results.extend(protein_analysis.aliphatic_index())
+		protein_results.extend(protein_analysis.gravy())
+		results[sequence_type] = protein_results
+		return results
+	return {'error': 'Invalid parameters', 'message': 'Invalid sequence type.'}
 
 
 def process_batch_folding(params):
@@ -58,7 +69,7 @@ def main(args):
 	in_fn = args[0]
 	out_fn = args[1]
 	result = process(_read_params(in_fn))
-	write_result(out_fn, result)
+	_write_result(out_fn, result)
 
 
 if __name__ == '__main__':
